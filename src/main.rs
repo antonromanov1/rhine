808aaa516c0c2ae87eaa1a2891dc3e82ce5611e0
Anton Romanov <antonromanov16@yandex.ru>
2021-05-07 15:58:02 +0300

    Created lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,1 @@
-mod ir;
-
-use crate::ir::Inst;
-
-fn main() {
-    let mut graph = ir::Graph::new();
-
-    let block = graph.create_empty_block();
-    let add = graph.create_inst_add();
-    let _not = graph.create_inst_not();
-    let _phi = graph.create_inst_phi();
-
-    unsafe {
-        (*block).add_inst(add, true);
-        (*add).dump();
-    }
-}
+fn main() {}
